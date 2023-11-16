541d351a85a5e822a99ff769efa3cc2085cc3caf
JBreece <joshbreece2012@gmail.com>
2023-11-16 00:43:40 -0500

    fixed gitignore

--- X/Intermediate/Build/Win64/x64/XEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.36.32543
-#include "D:/UnrealProjects/X/X/Intermediate/Build/Win64/x64/XEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h"
--- X/Intermediate/Build/Win64/x64/XEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "D:/UnrealProjects/X/X/Intermediate/Build/Win64/x64/XEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
